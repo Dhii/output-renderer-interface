17a0ce5b79c43b7f7bf415d6844c0b7cc2186e9e
Anton Ukhanev <xedin.unknown@gmail.com>
2016-11-26 19:52:03 +0100

    Added renderer interface

---
+++ src/RendererInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Output;
+
+/**
+ * Something that can render output.
+ *
+ * @since [*next-version*]
+ */
+interface RendererInterface
+{
+    /**
+     * Produce output based on the specifics of this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param $context The rendering context.
+     *  Something that can provide more information on how to perform the rendering.
+     *
+     * @return string|\Dhii\Util\String\StringableInterface The output.
+     */
+    public function render($context);
+}
