d3891ef8d1174e322b1fae232c36eabd3331c8da
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-11 11:43:17 +0200

    Added `RendererExceptionInterface`

---
+++ test/functional/Exception/RendererExceptionInterfaceTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dhii\Output\Exception\FuncTest;
+
+use Dhii\Output\Exception\RendererExceptionInterface;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Output\Exception\RendererExceptionInterface}.
+ *
+ * @since [*next-version*]
+ */
+class RendererExceptionInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Output\Exception\RendererExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return RendererExceptionInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->getRenderer();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject,
+            'Subject is not a valid instance'
+        );
+
+        $this->assertInstanceOf(
+            'Dhii\Output\RendererAwareInterface', $subject,
+            'Subject does not extend expected parent interface'
+        );
+    }
+}
