f701e31569cee4a6db11f8de80ac00447bf6f7ff
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-11 11:55:01 +0200

    Added `CouldNotRenderExceptionInterface`

---
+++ src/Exception/CouldNotRenderExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Output\Exception;
+
+/**
+ * Represents an exception thrown when the renderer fails to render the contents.
+ *
+ * @since [*next-version*]
+ */
+interface CouldNotRenderExceptionInterface extends RendererExceptionInterface
+{
+}
