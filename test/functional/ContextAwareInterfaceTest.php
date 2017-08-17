99a2124f9bb0104647ace8e7f48cfb2e18d9b1cd
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-17 18:26:21 +0200

    Added `ContextAwareInterface`

---
+++ test/functional/ContextAwareInterfaceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dhii\Output\FuncTest\FuncTest;
+
+use Dhii\Output\ContextAwareInterface;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see \Dhii\Output\FuncTest\ContextAwareInterface}.
+ *
+ * @since [*next-version*]
+ */
+class ContextAwareInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Output\FuncTest\ContextAwareInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return ContextAwareInterface
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->getContext()
+                     ->new();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'Subject is not a valid instance'
+        );
+    }
+}
