b828c3988a1fc39eaef41c460e2ab8afe9425785
Anton Ukhanev <xedin.unknown@gmail.com>
2017-09-02 13:37:23 +0200

    Added test for `CouldNotRenderExceptionInterface` (#3)

---
+++ test/unit/Exception/CouldNotRenderExceptionInterfaceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Dhii\Output\Exception\FuncTest;
+
+use Dhii\Output\Exception\CouldNotRenderExceptionInterface as TestSubject;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since 0.1
+ */
+class CouldNotRenderExceptionInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Output\Exception\CouldNotRenderExceptionInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                 ->getRenderable()
+                // ThrowableInterface
+                ->getMessage()
+                ->getCode()
+                ->getFile()
+                ->getLine()
+                ->getTrace()
+                ->getTraceAsString()
+                ->getPrevious()
+                ->__toString();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject,
+            'Could not create a valid instance of the test subject.'
+        );
+
+        $this->assertInstanceOf(
+            'Dhii\Output\Exception\RenderableAwareExceptionInterface', $subject,
+            'Subject does not extend expected parent interface'
+        );
+    }
+}
