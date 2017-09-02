17ff3fbf313ea3c78eaeb4ffd98729184f179c3f
Anton Ukhanev <xedin.unknown@gmail.com>
2017-09-02 13:26:23 +0200

    Added test for `RenderableAwareInterface` (#3)

---
+++ test/unit/RenderableAwareInterfaceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Dhii\Output\UnitTest;
+
+use Xpmock\TestCase;
+use Dhii\Output\RenderableAwareInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class RenderableAwareInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Output\RenderableAwareInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->getRenderable();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME, $subject,
+            'Could not create a valid instance of the test subject.'
+        );
+    }
+}
