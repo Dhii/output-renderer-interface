ad77a5e3b77601c6bcb76cd65d3c430dac1d6733
Anton Ukhanev <xedin.unknown@gmail.com>
2017-09-02 13:16:56 +0200

    Added `RenderableInterface` (#3)

---
+++ src/RenderableInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Output;
+
+/**
+ * Something that can produce output.
+ *
+ * @since [*next-version*]
+ */
+interface RenderableInterface
+{
+    /**
+     * Produces output.
+     *
+     * @since [*next-version*]
+     *
+     * @throws CouldNotRenderExceptionInterface If cannot render.
+     * @throws RendererExceptionInterface       Any other problem related to the renderer.
+     *
+     * @return string|Stringable The output.
+     */
+    public function render();
+}
