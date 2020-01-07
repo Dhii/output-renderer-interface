045fc08e4eb95fd6f8196ca257ee72de88a95a2e
Anton Ukhanev <xedin.unknown@gmail.com>
2020-01-07 09:54:15 +0100

    Fixed typo in class doc

--- src/Exception/CouldNotRenderExceptionInterface.php
+++ src/Exception/CouldNotRenderExceptionInterface.php
@@ -3,7 +3,7 @@
 namespace Dhii\Output\Exception;
 
 /**
- * Represents an exception thrown when the rendererer fails to render.
+ * Represents an exception thrown when the renderer fails to render.
  *
  * @since 0.1
  */
